0727fa236d61decfd990020cdbd7151db2a9158f
Mikko Suniala <mikko.suniala@iki.fi>
2021-06-28 22:38:34 +0300

    Ex 8.3 - 8.5, 8.13 - 8.15

--- src/test/kotlin/chapter8/exercises/ex13/listing.kt
+++ src/test/kotlin/chapter8/exercises/ex13/listing.kt
@@ -1,24 +1,32 @@
 package chapter8.exercises.ex13
 
+import arrow.core.extensions.list.foldable.exists
 import chapter8.Gen
 import chapter8.Prop
 import chapter8.SGen
 import chapter8.sec4_1.run
-import utils.SOLUTION_HERE
 
+/**
+Define nonEmptyListOf for generating nonempty lists, and then update your specification of
+max to use this generator.
+ */
 fun main() {
     //tag::init1[]
     fun <A> nonEmptyListOf(ga: Gen<A>): SGen<List<A>> =
-
-        SOLUTION_HERE()
+        SGen { size: Int ->
+            Gen.listOfN(Integer.max(size, 1), ga)
+        }
     //end::init1[]
 
     val smallInt = Gen.choose(-10, 10)
 
     //tag::init2[]
     fun maxProp(): Prop =
-
-        SOLUTION_HERE()
+        Prop.forAll(nonEmptyListOf(smallInt)) { ns: List<Int> ->
+            val mx = ns.max()
+                ?: throw IllegalStateException("max on empty list")
+            !ns.exists { it > mx }
+        }
     //end::init2[]
     run(maxProp())
 }
--- src/test/kotlin/chapter8/exercises/ex14/listing.kt
+++ src/test/kotlin/chapter8/exercises/ex14/listing.kt
@@ -1,15 +1,25 @@
 package chapter8.exercises.ex14
 
+import arrow.core.extensions.list.foldable.exists
 import chapter8.Gen
 import chapter8.Prop
+import chapter8.SGen
 import chapter8.sec4_1.run
-import utils.SOLUTION_HERE
 
 val smallInt = Gen.choose(-10, 10)
 
 fun List<Int>.prepend(i: Int) = listOf(i) + this
 
-fun maxProp(): Prop = SOLUTION_HERE()
+/**
+Write a property called maxProp to verify the behavior of List.sorted , which you can use to
+sort (among other things) a List<Int> .
+ */
+fun maxProp(): Prop =
+    Prop.forAll(SGen.listOf(smallInt)) { ns: List<Int> ->
+        val sorted = ns.sorted()
+        val consecutives = sorted.zipWithNext()
+        !consecutives.exists { (prev, next) -> prev > next }
+    }
 
 fun main() {
     run(maxProp())
