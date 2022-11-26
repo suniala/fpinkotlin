16d9123db41fbe7f86877787cbaf4a9a2ea2ad72
Mikko Suniala <mikko.suniala@iki.fi>
2022-11-26 17:25:26 +0200

    Ex 11.1, 11.3

--- src/test/kotlin/chapter11/exercises/ex1/listing.kt
+++ src/test/kotlin/chapter11/exercises/ex1/listing.kt
@@ -1,34 +1,91 @@
 package chapter11.exercises.ex1
 
+import arrow.Kind
 import arrow.core.ForListK
 import arrow.core.ForSequenceK
+import arrow.core.ListK
+import arrow.core.SequenceK
+import arrow.core.fix
 import chapter10.ForList
 import chapter10.ForOption
+import chapter10.List
+import chapter10.Option
+import chapter10.OptionOf
+import chapter10.fix
 import chapter11.ForPar
+import chapter11.Par
+import chapter11.fix
 import chapter11.sec2.Monad
-import utils.SOLUTION_HERE
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.WordSpec
 
 //tag::init1[]
+/**
+Write monad instances for Par , Option , and List . Additionally, provide monad instances
+for arrow.core.ListK and arrow.core.SequenceK .
+ */
 object Monads {
 
-    fun parMonad(): Monad<ForPar> =
+    fun parMonad(): Monad<ForPar> = object : Monad<ForPar> {
+        override fun <A> unit(a: A): Kind<ForPar, A> = Par.unit(a)
 
-        SOLUTION_HERE()
+        override fun <A, B> flatMap(
+            fa: Kind<ForPar, A>,
+            f: (A) -> Kind<ForPar, B>
+        ): Kind<ForPar, B> = fa.fix().flatMap { f(it).fix() }
+    }
 
-    fun optionMonad(): Monad<ForOption> =
+    fun optionMonad(): Monad<ForOption> = object : Monad<ForOption> {
+        override fun <A> unit(a: A): OptionOf<A> = Option.of(a)
 
-        SOLUTION_HERE()
+        override fun <A, B> flatMap(
+            fa: OptionOf<A>,
+            f: (A) -> OptionOf<B>
+        ): OptionOf<B> = fa.fix().flatMap { f(it).fix() }
+    }
 
-    fun listMonad(): Monad<ForList> =
+    fun listMonad(): Monad<ForList> = object : Monad<ForList> {
+        override fun <A> unit(a: A): Kind<ForList, A> = List.of(a)
 
-        SOLUTION_HERE()
+        override fun <A, B> flatMap(
+            fa: Kind<ForList, A>,
+            f: (A) -> Kind<ForList, B>
+        ): Kind<ForList, B> = fa.fix().flatMap { f(it).fix() }
+    }
 
-    fun listKMonad(): Monad<ForListK> =
+    fun listKMonad(): Monad<ForListK> = object : Monad<ForListK> {
+        override fun <A> unit(a: A): Kind<ForListK, A> = ListK.just(a)
 
-        SOLUTION_HERE()
+        override fun <A, B> flatMap(
+            fa: Kind<ForListK, A>,
+            f: (A) -> Kind<ForListK, B>
+        ): Kind<ForListK, B> = fa.fix().flatMap { f(it).fix() }
+    }
 
     fun sequenceKMonad(): Monad<ForSequenceK> =
+        object : Monad<ForSequenceK> {
+            override fun <A> unit(a: A): Kind<ForSequenceK, A> =
+                SequenceK.just(a)
 
-        SOLUTION_HERE()
+            override fun <A, B> flatMap(
+                fa: Kind<ForSequenceK, A>,
+                f: (A) -> Kind<ForSequenceK, B>
+            ): Kind<ForSequenceK, B> = fa.fix().flatMap { f(it).fix() }
+        }
 }
 //end::init1[]
+
+class Exercise1 : WordSpec({
+    "monads" should {
+        "option" {
+            val monad = Monads.optionMonad()
+            monad.unit("1") shouldBe Option.of("1")
+            monad.map(Option.of(42)) { v -> "foo-$v" } shouldBe Option.of("foo-42")
+            monad.flatMap(Option.of(42)) { v -> Option.of("foo-$v") } shouldBe Option.of(
+                "foo-42"
+            )
+            monad.flatMap(Option.of(42)) { Option.empty<String>() } shouldBe Option.empty()
+            monad.flatMap(Option.empty<Int>()) { v -> Option.of("foo-$v") } shouldBe Option.empty()
+        }
+    }
+})
--- src/test/kotlin/chapter11/exercises/ex3/listing.kt
+++ src/test/kotlin/chapter11/exercises/ex3/listing.kt
@@ -1,10 +1,16 @@
 package chapter11.exercises.ex3
 
 import arrow.Kind
+import chapter10.Cons
 import chapter10.List
+import chapter10.ListFoldable.foldRight
 import chapter11.Functor
-import utils.SOLUTION_HERE
 
+/**
+The sequence and traverse combinators should be pretty familiar to you by now, and
+your implementations of them from previous chapters are probably all very similar.
+Implement them once and for all on Monad<F> .
+ */
 interface Monad<F> : Functor<F> {
 
     fun <A> unit(a: A): Kind<F, A>
@@ -20,14 +26,27 @@ interface Monad<F> : Functor<F> {
     fun <A, B> traverse(
         la: List<A>,
         f: (A) -> Kind<F, B>
-    ): Kind<F, List<B>> =
+    ): Kind<F, List<B>> = when (la) {
+        is Cons<A> -> {
+            foldRight(la, unit(List.empty())) { a, lb ->
+                flatMap(lb) { acc: List<B> ->
+                    map(f(a)) { b ->
+                        Cons(b, acc)
+                    }
+                }
+            }
+        }
 
-        SOLUTION_HERE()
+        else -> {
+            unit(List.empty())
+        }
+    }
     //end::traverse[]
 
     //tag::sequence[]
     fun <A> sequence(lfa: List<Kind<F, A>>): Kind<F, List<A>> =
-
-        SOLUTION_HERE()
+        traverse(lfa) {
+            it
+        }
     //end::sequence[]
 }
