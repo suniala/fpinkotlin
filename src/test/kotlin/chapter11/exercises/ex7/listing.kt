849818e0b16265b4c1568bb4f0e292ba3463563c
Mikko Suniala <mikko.suniala@iki.fi>
2022-11-29 21:06:45 +0200

    Ex 11.7 - 11.8

--- src/test/kotlin/chapter11/exercises/ex7/listing.kt
+++ src/test/kotlin/chapter11/exercises/ex7/listing.kt
@@ -2,8 +2,10 @@ package chapter11.exercises.ex7
 
 import arrow.Kind
 import chapter11.Functor
-import utils.SOLUTION_HERE
 
+/**
+Implement the following Kleisli composition function in Monad :
+ */
 interface Monad<F> : Functor<F> {
 
     fun <A> unit(a: A): Kind<F, A>
@@ -13,8 +15,8 @@ interface Monad<F> : Functor<F> {
     fun <A, B, C> compose(
         f: (A) -> Kind<F, B>,
         g: (B) -> Kind<F, C>
-    ): (A) -> Kind<F, C> =
-
-        SOLUTION_HERE()
+    ): (A) -> Kind<F, C> = { a ->
+        flatMap(f(a)) { b -> g(b) }
+    }
     //end::init[]
 }
--- src/test/kotlin/chapter11/exercises/ex8/listing.kt
+++ src/test/kotlin/chapter11/exercises/ex8/listing.kt
@@ -2,11 +2,17 @@ package chapter11.exercises.ex8
 
 import arrow.Kind
 import chapter11.Functor
-import utils.SOLUTION_HERE
 
+/**
+Hard: Implement flatMap in terms of an abstract definition of compose . By this, it
+seems as though we’ve found another minimal set of monad combinators: compose
+and unit .
+ */
 interface Monad<F> : Functor<F> {
 
     fun <A> unit(a: A): Kind<F, A>
+
+    override fun <A, B> map(fa: Kind<F, A>, f: (A) -> B): Kind<F, B>
 
     fun <A, B, C> compose(
         f: (A) -> Kind<F, B>,
@@ -17,8 +23,9 @@ interface Monad<F> : Functor<F> {
     fun <A, B> flatMap(
         fa: Kind<F, A>,
         f: (A) -> Kind<F, B>
-    ): Kind<F, B> =
-
-        SOLUTION_HERE()
+    ): Kind<F, B> {
+        val composed = compose<Unit, A, B>({ fa }, f)
+        return composed(Unit)
+    }
     //end::init[]
 }
